10805dcfd2afca724e1cd4f1bf951001df93b503
Michael Refvem <mkrefvem@gmail.com>
2020-07-21 14:04:06 -0700

    Async Inn Management System, application specifications: Startup File, Simple Models & The Database, Seeded Data

---
+++ AsyncInn/AsyncInn/Data/AsyncInnDBContext.cs
@@ -0,0 +1,100 @@
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Seattle Marriott Redmond",
+                    StreetAddress = "7401 164th Ave NE",
+                    City = "Redmond",
+                    State = "Washington",
+                    Phone = "+1 (425) 498-4000"
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Seattle Marriott Waterfront",
+                    StreetAddress = "2100 Alaskan Way",
+                    City = "Seattle",
+                    State = "Washington",
+                    Phone = "+1 (206) 443-5000"
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Seattle Marriott Bellevue",
+                    StreetAddress = "200 110th Ave NE",
+                    City = "Bellevue",
+                    State = "Washington",
+                    Phone = "+1 (425) 214-7600"
+                }
+                );
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    Id = 1,
+                    Name = "Studio",
+                    Layout = 0
+                },
+                new Room
+                {
+                    Id = 2,
+                    Name = "One Bedroom",
+                    Layout = 1
+                },
+                new Room
+                {
+                    Id = 3,
+                    Name = "Two Bedroom",
+                    Layout = 2
+                }
+                );
+            modelBuilder.Entity<Amenity>().HasData(
+                new Amenity
+                {
+                    Id = 1,
+                    Name = "Microwave"
+                },
+                new Amenity
+                {
+                    Id = 2,
+                    Name = "Television"
+                },
+                new Amenity
+                {
+                    Id = 3,
+                    Name = "Mini Safe"
+                }
+                );
+        }
+
+        // to create an initial migration
+        // Install-Package Microsoft.EntityFrameworkCore.Tools
+        // add-migration {migration name}
+        // add-migration initial
+        // "add-migration addCoursesSimpleModel"
+        // then update my DB and see the new table added
+        // "add-migration addCoursesSeededData"
+        // run "update-database"
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+    }
+}
---
+++ AsyncInn/AsyncInn/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using AsyncInn.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AsyncInn
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        // the appsettings.json by default is our "Configurations" for the app.
+        // Set ourselves up for dependency injection
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // this is where all of our dependencies are going to live
+            // enable the use of using controllers within the MVC convention
+            services.AddControllers();
+
+            // Register with the app that the Db exists and what options to use for it
+            services.AddDbContext<AsyncInnDbContext>(options =>
+            {
+                // Install Package Microsoft.EntityFrameworkCore.SqlServer
+                // Connection String = location where something lives. In our case, it's where our DB lives.
+                // Connection string contains the location, username, pw of your sql server...with our sql database directly
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Set our default routing for our request
+                endpoints.MapControllerRoute("default", "{controller-Home}/{action-Index}/{id?}");
+            });
+        }
+    }
+}
